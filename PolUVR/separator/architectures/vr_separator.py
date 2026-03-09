f499d674b6624a61c13f76bf9adc895db53db9e3
Artyom Bebroy <artyom.vasilenko.99@list.ru>
2026-03-09 16:40:21 +0500

    Minor file synchronization with `python-audio-separator`

--- PolUVR/separator/architectures/vr_separator.py
+++ PolUVR/separator/architectures/vr_separator.py
@@ -138,8 +138,8 @@ class VRSeparator(CommonSeparator):
 
         self.model_run = lambda *args, **kwargs: self.logger.error("Model run method is not initialised yet.")
 
-        # This should go away once we refactor to remove soundfile.write and replace with pydub like we did for the MDX rewrite
-        self.wav_subtype = "PCM_16"
+        # wav_subtype will be set based on input audio bit depth in prepare_mix()
+        # Removed hardcoded "PCM_16" to allow bit depth preservation
 
         self.logger.info("VR Separator initialisation complete")
 
@@ -161,6 +161,32 @@ class VRSeparator(CommonSeparator):
         self.audio_file_path = audio_file_path
         self.audio_file_base = os.path.splitext(os.path.basename(audio_file_path))[0]
 
+        # Detect input audio bit depth for output preservation
+        try:
+            import soundfile as sf
+            info = sf.info(audio_file_path)
+            self.input_audio_subtype = info.subtype
+            self.logger.info(f"Input audio subtype: {self.input_audio_subtype}")
+            
+            # Map subtype to wav_subtype for soundfile and set input_bit_depth for pydub
+            if "24" in self.input_audio_subtype:
+                self.wav_subtype = "PCM_24"
+                self.input_bit_depth = 24
+                self.logger.info("Detected 24-bit input audio")
+            elif "32" in self.input_audio_subtype:
+                self.wav_subtype = "PCM_32"
+                self.input_bit_depth = 32
+                self.logger.info("Detected 32-bit input audio")
+            else:
+                self.wav_subtype = "PCM_16"
+                self.input_bit_depth = 16
+                self.logger.info("Detected 16-bit input audio")
+        except Exception as e:
+            self.logger.warning(f"Could not detect input audio bit depth: {e}. Defaulting to PCM_16")
+            self.wav_subtype = "PCM_16"
+            self.input_audio_subtype = None
+            self.input_bit_depth = 16
+
         self.logger.debug(f"Starting separation for input audio file {self.audio_file_path}...")
 
         nn_arch_sizes = [31191, 33966, 56817, 123821, 123812, 129605, 218409, 537238, 537227]  # default
@@ -177,7 +203,7 @@ class VRSeparator(CommonSeparator):
             self.logger.debug("Determining model capacity...")
             self.model_run = nets.determine_model_capacity(self.model_params.param["bins"] * 2, nn_arch_size)
 
-        self.model_run.load_state_dict(torch.load(self.model_path, map_location=self.torch_device_cpu))
+        self.model_run.load_state_dict(torch.load(self.model_path, map_location="cpu"))
         self.model_run.to(self.torch_device)
         self.logger.debug("Model loaded and moved to device.")
 
